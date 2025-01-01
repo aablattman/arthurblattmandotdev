0aec35f4e70cf2f4e3d54fc3b7f78a8ccf7b96da
Arthur Blattman <aablattman@gmail.com>
2025-01-01 08:36:44 -0600

    ssr/csr match

--- web/app/components/AabddTopModule.js
+++ web/app/components/AabddTopModule.js
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box';
 
 export default function AabddTopModule() {
 	return (
-		<Box sx={{ mt: 2, ml: 2, mr: 2, p: 2, border: '1px dashed grey'}}>
+		<Box className="aabdd-top-module">
 			arthurblattman.dev
 		</Box>
 	);
