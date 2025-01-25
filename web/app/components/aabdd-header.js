6a64085c7ee71d2a429c75307a752ea015b2c909
Arthur Blattman <aablattman@gmail.com>
2025-01-25 11:56:21 -0600

    order

--- web/app/components/aabdd-header.js
+++ web/app/components/aabdd-header.js
@@ -10,12 +10,14 @@ export default function AabddHeader() {
 		<Box className="aabdd-header">
 			<span>arthurblattman.dev</span>
 			<Box className="header-icons">
-				<Link className="leetcode-link" href="https://leetcode.com/u/aablattman/" passHref>
-					<Image src="/leetcode.png" alt="LeetCode Icon" width={24} height={24} />
-				</Link>
 				<Link className="github-link" href="https://pickcrunch.com" passHref>
 					<Image src="/PickCrunch-Icon-RGB.png" alt="Pick Crunch Icon" width={24} height={24} />
 				</Link>
+
+				<Link className="leetcode-link" href="https://leetcode.com/u/aablattman/" passHref>
+					<Image src="/leetcode.png" alt="LeetCode Icon" width={24} height={24} />
+				</Link>
+
 				<Link className="github-link" href="https://github.com/aablattman/arthurblattmandotdev" passHref>
 					<Image src="/github-mark-white.png" alt="GitHub Icon" width={24} height={24} />
 				</Link>
